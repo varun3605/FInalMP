c8a2133b7fe407f6b600187dbdcb3175e26ee496
Varun <varun3605@gmail.com>
2018-05-13 23:09:18 +0530

    Navigation Drawer and Search Bar Added

--- app/src/main/java/com/hvr/music/vaanimusicplayer/HomeActivity.java
+++ app/src/main/java/com/hvr/music/vaanimusicplayer/HomeActivity.java
@@ -1,5 +1,10 @@
 package com.hvr.music.vaanimusicplayer;
 
+import android.app.SearchManager;
+import android.content.Context;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
@@ -7,28 +12,41 @@ import android.support.design.widget.TabLayout;
 import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.SearchView;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 
 public class HomeActivity extends AppCompatActivity {
 
-    FragmentAdapter mFragmentAdapter;
-    ViewPager mViewPager;
-    TabLayout mTabLayout;
-    String[] TabNames;
-    Toolbar mToolbar;
+    private FragmentAdapter mFragmentAdapter;
+    private ViewPager mViewPager;
+    private TabLayout mTabLayout;
+    private ActionBar mActionBar;
+    private String[] TabNames;
+    private Toolbar mToolbar;
+    private DrawerLayout mDrawerLayout;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
         mToolbar = findViewById(R.id.toolbar);
         setSupportActionBar(mToolbar);
 
+        mActionBar = getSupportActionBar();
+        mActionBar.setDisplayHomeAsUpEnabled(true);
+        mActionBar.setHomeAsUpIndicator(R.drawable.ic_menu_24dp);
+
         mViewPager = findViewById(R.id.pager);
         mTabLayout = findViewById(R.id.tabL);
+        mDrawerLayout = findViewById(R.id.DrawerRoot);
 
         mFragmentAdapter = new FragmentAdapter(getSupportFragmentManager());
-
         mViewPager.setAdapter(mFragmentAdapter);
 
         TabNames = getResources().getStringArray(R.array.HomeScreen);
@@ -36,6 +54,7 @@ public class HomeActivity extends AppCompatActivity {
         for (String TabName : TabNames) {
             mTabLayout.addTab(mTabLayout.newTab().setText(TabName));
         }
+
         mTabLayout.addOnTabSelectedListener(new TabLayout.OnTabSelectedListener() {
             @Override
             public void onTabSelected(TabLayout.Tab tab) {
@@ -70,5 +89,27 @@ public class HomeActivity extends AppCompatActivity {
             }
         });
     }
-}
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.search, menu);
+
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        SearchView searchView = (SearchView) menu.findItem(R.id.search_button).getActionView();
+        searchView.setSearchableInfo(searchManager.getSearchableInfo(getComponentName()));
+
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId())
+        {
+            case android.R.id.home:
+                mDrawerLayout.openDrawer(GravityCompat.START);
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
